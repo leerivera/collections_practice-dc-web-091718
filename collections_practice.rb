9f1023c3909713bb18ebd356814032ab15553628
Nick Rivera <leerivera80@gmail.com>
2018-09-27 14:22:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+def sort_array_asc(array)
+  array.sort
+  
+  
+end
 
+def sort_array_desc(array)
+  array.sort_by {|str| -str}
+end
+
+
+def sort_array_char_count(array)
+  array.sort_by{ |word| word.length }
+    
+    
+ 
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+   a.length <=> b.length
+ end
+  
+end
+
+def reverse_array(array)
+  array.reverse
+
+end
+
+def kesha_maker(array)
+  array.each{|word| }
+   
+  
+end
+
+def find_a(array)
+  array.select { |word| word.start_with?('a') }
+end
+
+def sum_array(array)
+  array.reduce(:+)
+
+  
+end
+
+def add_s(array)
+  array.each_with_index.collect{|element, index|}
+   index.concat "a"
+ 
+
+
+end
